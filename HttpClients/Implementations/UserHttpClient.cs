d113cb33fc79de3eb06de0ea09def6be30d848b6
ZbyslawMateo <mateusz.zbyslaw@gmail.com>
2022-10-21 20:53:34 +0200

    Blazor add user component running and tested

---
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -0,0 +1,32 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using HttpClients.ClientInterfaces;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.Implementations;
+
+public class UserHttpClient : IUserService
+{
+    private readonly HttpClient client;
+
+    public UserHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+    public async Task<User> Create(UserCreationDto dto)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("/user", dto);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return user;
+    }
+}
